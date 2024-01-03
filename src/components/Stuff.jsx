c527c3a5d9f419b8ab8dc9198a5520f284ca96a4
bssbrit <brunosabrito2@gmail.com>
2024-01-03 18:05:12 -0300

    Made the profile css properties

--- src/components/Stuff.jsx
+++ src/components/Stuff.jsx
@@ -3,23 +3,21 @@ const Stuff = (data) => {
 
   if (dataArr != null) {
     return (
-      <div>
-        <ul>
-          {dataArr.map((item) => (
-            <div key={item.id}>
-              <p key={item.id}>{item.title}</p>
-              <p key={item.id}>{item.timeframes.current}hrs</p>
+      <ul id="info">
+        {dataArr.map((item) => (
+          <div key={item.id}>
+            <p key={item.id}>{item.title}</p>
+            <p key={item.id}>{item.timeframes.current}hrs</p>
 
-              <p key={item.id}>
-                Last{" "}
-                {item.choosenTime.charAt(0).toUpperCase() +
-                  item.choosenTime.slice(1)}{" "}
-                - {item.timeframes.previous}hrs
-              </p>
-            </div>
-          ))}
-        </ul>
-      </div>
+            <p key={item.id}>
+              Last{" "}
+              {item.choosenTime.charAt(0).toUpperCase() +
+                item.choosenTime.slice(1)}{" "}
+              - {item.timeframes.previous}hrs
+            </p>
+          </div>
+        ))}
+      </ul>
     );
   } else return <div>No Tasks done</div>;
 };
