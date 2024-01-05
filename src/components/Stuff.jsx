b9b894f7d2279c257b523d45a8bc768514f8990a
bssbrit <brunosabrito2@gmail.com>
2024-01-04 22:57:48 -0300

    finishing the style

--- src/components/Stuff.jsx
+++ src/components/Stuff.jsx
@@ -1,23 +1,35 @@
+import ellipsisIcon from "../../assets/images/icon-ellipsis.svg";
 const Stuff = (data) => {
   const dataArr = data.data;
 
   if (dataArr != null) {
     return (
-      <ul id="info">
+      <div id="info">
         {dataArr.map((item) => (
-          <div key={item.id}>
-            <p key={item.id}>{item.title}</p>
-            <p key={item.id}>{item.timeframes.current}hrs</p>
+          <div
+            key={item.id}
+            className="bigActivity"
+            id={item.title.split(" ").join("_")}
+          >
+            <div key={item.id} className="activities">
+              <p key={item.id}>
+                {item.title}
+                <p>
+                  <img src={ellipsisIcon} alt="" />
+                </p>
+              </p>
+              <p key={item.id}>{item.timeframes.current}hrs</p>
 
-            <p key={item.id}>
-              Last{" "}
-              {item.choosenTime.charAt(0).toUpperCase() +
-                item.choosenTime.slice(1)}{" "}
-              - {item.timeframes.previous}hrs
-            </p>
+              <p key={item.id}>
+                Last{" "}
+                {item.choosenTime.charAt(0).toUpperCase() +
+                  item.choosenTime.slice(1)}{" "}
+                - {item.timeframes.previous}hrs
+              </p>
+            </div>
           </div>
         ))}
-      </ul>
+      </div>
     );
   } else return <div>No Tasks done</div>;
 };
